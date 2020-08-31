690911c16363826a4fbb43f498c2d5ac4dba4ae1
Philippa Olomoro <olomoro.philippa@gmail.com>
2020-08-31 11:04:21 +0100

    another commit

--- savestproject/settings.py
+++ savestproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&*eux&5(kys6^41br%xq=n$&l)dcj#mmfqa3o(_*%q0@v^qlc('
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
